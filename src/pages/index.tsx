dbfb5249c9da25749e1c887d58d912d304b3c77f
bbruscatto <bruscatto@gmail.com>
2022-04-28 15:25:31 -0300

    fix: codigo publication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -131,7 +131,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const posts = postsResponse.results.map(post => {
     return {
       uid: post.uid,
-      first_publication_date: post.data.first_publication_date,
+      first_publication_date: post.first_publication_date,
       data: {
         title: post.data.title,
         subtitle: post.data.subtitle,
